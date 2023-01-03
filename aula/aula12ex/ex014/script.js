ea7df9912f69e6cdf1ff81184faedc4c4d1c53bf
Kelly Rodrigues <118572924+KellyRodrigues85@users.noreply.github.com>
2023-01-03 16:50:05 -0300

    funcao hora

--- aula/aula12ex/ex014/script.js
+++ aula/aula12ex/ex014/script.js
@@ -1,8 +1,17 @@
-function carregar( ) {
-    var txtv = document.querySelector('div#msg')
-    var res = document.querySelector('div#imgagem')
+
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
+    if  (hora >= 0 && hora < 12) {
+        img.src="imagens/manha2.jpg"
+        
+    } else if (hora >=  12 && hora < 18){
+        img.src="imagens/tarde2.jpg"
+    } else {
+        img.src="imagens/noite1.jpg"
+    }
 
 }
