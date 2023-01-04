84460bf3951e8f3f85b527cb7f2975901ced58b6
Kelly Rodrigues <118572924+KellyRodrigues85@users.noreply.github.com>
2023-01-04 11:52:31 -0300

    inserindo imagens

--- aula/aula12ex/ex015/script.js
+++ aula/aula12ex/ex015/script.js
@@ -3,13 +3,46 @@ function verificar() {
    var ano = data.getFullYear()
    var fano = document.getElementById('txtano')
    var res = document.getElementById('res')
+   var img = document.createElement('img')
+   img.setAttribute('id','foto')
 
    if(fano.value.length == 0 || Number(fano.value) > ano ){
          window.alert('verifique os dados e tente novamente')
    } else {
         var fsex = document.getElementsByName ('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML= `Idade caulculada: ${idade}`
+        var genero = ''
+        if (fsex[0].checked){
+                genero = 'Homem'
+                if (idade >= 0 && idade < 10 ) {
+                        img.setAttribute('src', 'm_crianca.jpg')    
+                } else if (idade < 21){
+                        img.setAttribute('src', 'm_adolescente.jpg') 
+
+                } else if (idade < 60){
+                        img.setAttribute('src', 'm_adulto.jpg')
+
+                } else (idade > 60 ) {
+                        img.setAttribute('src', 'm_idoso.jpg')
+                }       
+
+        }else if (fsex[1].checked) {
+                genero = 'Mulher'
+
+                if (idade >= 0 && idade <10 ) {
+                        //criança
+                } else if (idade < 21){
+                 //jovem
+
+                } else if (idade < 60){
+                        // adulto
+
+                } else (idade > 60 )
+                 // idoso 
+        }
+        res.style.textAlign= 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
    }
        
 
