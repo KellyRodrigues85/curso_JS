c54e499edb1cda81c396a51a5a812257303b9b2b
Kelly Rodrigues <kellyrodrigues8519@gmail.com>
2023-01-10 18:04:36 -0300

    function

---
+++ aula/ex014/ex016/Modelo/script.js
@@ -0,0 +1,12 @@
+function contar () {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+
+    if(ini.value.length == 0 || fim.value.length == 0|| passo.value.length == 0) {
+        window.alert ("Erro- faltam dados!")
+    } else {
+        alert('tudo ok !')
+    }
+
+}
