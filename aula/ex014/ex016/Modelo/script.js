2635ed3132b6e8885709fd0d0ce46458dc72ca2a
Kelly Rodrigues <kellyrodrigues8519@gmail.com>
2023-01-13 17:48:28 -0300

    config html

--- aula/ex014/ex016/Modelo/script.js
+++ aula/ex014/ex016/Modelo/script.js
@@ -7,7 +7,14 @@ function contar () {
     if ( ini.value.length == 0 || fim.value.length ==0 || passo.value.length ==0 ){
         window.alert ('Erro - faltam dados!')
     } else{
-        window.alert('tudo ok')
+        res.innerHTML = 'Contando:'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for(let c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c}`;
+        }
     }
 
 
