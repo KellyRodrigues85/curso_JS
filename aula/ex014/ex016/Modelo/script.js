b857948bc627cb842c83750af320e3f6b1c0df43
Kelly Rodrigues <kellyrodrigues8519@gmail.com>
2023-01-13 16:50:03 -0300

    testando a funçao

--- aula/ex014/ex016/Modelo/script.js
+++ aula/ex014/ex016/Modelo/script.js
@@ -1,23 +1,14 @@
 function contar () {
-    var ini = document.getElementById('txti')
-    var fim = document.getElementById('txtf')
-    var passo = document.getElementById('txtp')
-    var res = document.getElementById('res')
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert ("Erro- faltam dados!")
-    } else {
-        res.innerHTML = 'contando:'
-        var i = Number(ini.value)
-        var f = Number(fim.value)
-        var p = Number(passo.value)
-        var pe = Number
-
-        for(var c = i; c <= f; c += p) {
-            res.innerHTML += `${c}`
-        }
-                	
-        
+    if ( ini.value.length == 0 || fim.value.length ==0 || passo.value.length ==0 ){
+        window.alert ('Erro - faltam dados!')
+    } else{
+        window.alert('tudo ok')
     }
+
 
 }
